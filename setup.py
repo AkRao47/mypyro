055e80cf4c5984f8bd07b464daa1d78401f68021
AkRao47 <159660084+AkRao47@users.noreply.github.com>
2024-06-07 16:03:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,52 +1,40 @@
-#  Pyrogram - Telegram MTProto API Client Library for Python
-#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
-#
-#  This file is part of Pyrogram.
-#
-#  Pyrogram is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Lesser General Public License as published
-#  by the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  Pyrogram is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public License
-#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
-
+# setup.py
 import re
 from sys import argv
-
 from setuptools import setup, find_packages
 
+# Import your compilers if necessary
 from compiler.api import compiler as api_compiler
 from compiler.errors import compiler as errors_compiler
 
+# Read the requirements from requirements.txt
 with open("requirements.txt", encoding="utf-8") as r:
     requires = [i.strip() for i in r]
 
-with open("pyrogram/__init__.py", encoding="utf-8") as f:
-    version = re.findall(r"__version__ = \"(.+)\"", f.read())[0]
+# Extract the version number from the package __init__.py file
+with open("MYPyrogram/__init__.py", encoding="utf-8") as f:
+    version = re.findall(r'__version__ = "(.+)"', f.read())[0]
 
+# Read the long description from README.md
 with open("README.md", encoding="utf-8") as f:
     readme = f.read()
 
+# Compile if specific commands are passed
 if len(argv) > 1 and argv[1] in ["bdist_wheel", "install", "develop"]:
     api_compiler.start()
     errors_compiler.start()
 
+# Setup configuration
 setup(
-    name="MYPyrogram",
+    name="MYPyrogram",  # Updated package name
     version=version,
     description="Elegant, modern and asynchronous Telegram MTProto API framework in Python for users and bots",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/pyrogram",
-    download_url="https://github.com/pyrogram/pyrogram/releases/latest",
-    author="Dan",
-    author_email="dan@pyrogram.org",
+    url="https://github.com/yourusername/MYPyrogram",  # Update to your repo URL
+    download_url="https://github.com/yourusername/MYPyrogram/releases/latest",  # Update to your repo URL
+    author="Your Name",  # Update with your name
+    author_email="your.email@example.com",  # Update with your email
     license="LGPLv3",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -73,14 +61,14 @@ setup(
     ],
     keywords="telegram chat messenger mtproto api client library python",
     project_urls={
-        "Tracker": "https://github.com/pyrogram/pyrogram/issues",
-        "Community": "https://t.me/pyrogram",
-        "Source": "https://github.com/pyrogram/pyrogram",
-        "Documentation": "https://docs.pyrogram.org",
+        "Tracker": "https://github.com/yourusername/MYPyrogram/issues",  # Update to your repo URL
+        "Community": "https://t.me/MYPyrogram",  # Update to your community link
+        "Source": "https://github.com/yourusername/MYPyrogram",  # Update to your repo URL
+        "Documentation": "https://docs.MYPyrogram.org",  # Update to your documentation URL
     },
     python_requires="~=3.7",
     package_data={
-        "mypyrogram": ["py.typed"],
+        "MYPyrogram": ["py.typed"],  # Update to the new package name
     },
     packages=find_packages(exclude=["compiler*", "tests*"]),
     zip_safe=False,
